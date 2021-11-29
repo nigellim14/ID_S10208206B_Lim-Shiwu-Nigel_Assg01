3fd35798c189e3ca979de65586323a6303fa6433
nigellim14 <S10208206@connect.np.edu.sg>
2021-11-30 01:17:32 +0800

    Created a button using Javascript function.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const d = new Date();
 document.getElementById("date").innerHTML = d;
 
+function myFunction() {
+    var txt;
+    if (confirm("Are you ready to move to the next page and continue to get to know me more?")) {
+      txt = "Nice! Now you got proceed to the top navigation to continue know more about me in the the next page";
+    } else {
+      txt = "Why not? I have some interesting skills that you should know in my skills page!";
+    }
+    document.getElementById("test").innerHTML = txt;
+  }
