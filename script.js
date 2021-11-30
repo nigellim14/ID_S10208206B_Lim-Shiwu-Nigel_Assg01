48e817b66d164bbfae32d024904db26655bad691
nigellim14 <S10208206@connect.np.edu.sg>
2021-11-30 17:12:57 +0800

    Enhanced page features, inserting validation.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Current Date & Time
 const d = new Date();
 document.getElementById("date").innerHTML = d;
 
+// Index page button interacting with user
 function myFunction() {
     var txt;
     if (confirm("Are you ready to move to the next page and continue to get to know me more?")) {
@@ -10,3 +12,28 @@ function myFunction() {
     }
     document.getElementById("test").innerHTML = txt;
   }
+
+
+// Validation for contact form
+function validateForm() {
+  var x = document.forms["myForm"]["name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  var x = document.forms["myForm"]["email"].value;
+  if (x == "") {
+    alert("Email must be filled out");
+    return false;
+}
+
+var x = document.forms["myForm"]["comment"].value;
+  if (x == "") {
+    alert("You have forgotten to filled up the comment section.");
+    return false;
+  }
+}
+
+
+
+  
